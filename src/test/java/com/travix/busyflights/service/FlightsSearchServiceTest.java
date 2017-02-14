68957d98adee823cd7ec56af143405ac1e3843e2
bosz <bobuskyyserhiy@gmail.com>
2017-02-14 08:05:51 +0000

    flights

--- src/test/java/com/travix/busyflights/service/FlightsSearchServiceTest.java
+++ src/test/java/com/travix/busyflights/service/FlightsSearchServiceTest.java
@@ -1,14 +1,15 @@
 package com.travix.busyflights.service;
 
-import com.google.common.collect.Lists;
 import com.travix.busyflights.domain.Flight;
+import com.travix.busyflights.service.rest.dto.CrazyAirFlightDto;
+import com.travix.busyflights.service.rest.dto.CrazyAirSearchRequest;
+import com.travix.busyflights.service.rest.dto.ToughJetFlightDto;
+import com.travix.busyflights.service.rest.dto.ToughJetSearchRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Calendar;
@@ -18,11 +19,14 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by sergej on 13.2.2017.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class FlightsSearchServiceTest {
 
     private static final String ORIGIN = "LDN";
@@ -31,40 +35,34 @@ public class FlightsSearchServiceTest {
     private static final String SEARCH_URL_CRAZY_AIR = "http://localhost:8080/flightsSearch/crayzAir";
     private static final String SEARCH_URL_TOUGH_JET = "http://localhost:8080/flightsSearch/toughJet";
 
+    @Mock
     private RestTemplate restTemplate;
     @Mock
-    private Flight flight_ldn2Mlb;
+    private CrazyAirFlightDto crazyAirFlightDto;
     @Mock
-    private Flight flight_mlb2Ldn;
+    private ToughJetFlightDto toughJetFlightDto;
     private SearchCriteria searchCriteria;
     private List<Flight> flights;
     private FlightsSearchService searchService;
 
     @Before
     public void setUp() {
-        restTemplate = new RestTemplate();
         searchService = new FlightsSearchService(restTemplate, SEARCH_URL_CRAZY_AIR, SEARCH_URL_TOUGH_JET);
     }
 
     @Test
     public void getFlights() {
+        givenARestTemplate();
         givenASearchCriteria();
         whenICallGetFlights();
         flightsAreReturned();
     }
-//    @Test
-//    public void getFlights() {
-//        givenARestTemplate();
-//        givenASearchCriteria();
-//        whenICallGetFlights();
-//        flightsAreReturned();
-//    }
 
     private void givenARestTemplate() {
-        List<Flight> orig2dest = Lists.newArrayList(flight_ldn2Mlb);
-        List<Flight> dest2orig = Lists.newArrayList(flight_mlb2Ldn);
-//        when(restTemplate.findCrazyAirFlights(eq(ORIGIN), eq(DESTINATION), any(Date.class), any(Date.class))).thenReturn(orig2dest);
-//        when(restTemplate.findCrazyAirFlights(eq(DESTINATION), eq(ORIGIN), any(Date.class), any(Date.class))).thenReturn(dest2orig);
+        CrazyAirFlightDto[] crazyAirList = new CrazyAirFlightDto[]{crazyAirFlightDto};
+        ToughJetFlightDto[] toughJetList = new ToughJetFlightDto[]{toughJetFlightDto};
+        when(restTemplate.postForObject(eq(SEARCH_URL_CRAZY_AIR), any(CrazyAirSearchRequest.class), any(Class.class))).thenReturn(crazyAirList);
+        when(restTemplate.postForObject(eq(SEARCH_URL_TOUGH_JET), any(ToughJetSearchRequest.class), any(Class.class))).thenReturn(toughJetList);
     }
 
     private void givenASearchCriteria() {
@@ -82,8 +80,6 @@ public class FlightsSearchServiceTest {
 
     private void flightsAreReturned() {
         assertThat(flights.size(), is(2));
-//        assertTrue(flights.contains(flight_ldn2Mlb));
-//        assertTrue(flights.contains(flight_mlb2Ldn));
     }
 
     private Date toDate(int year, int month, int day) {
